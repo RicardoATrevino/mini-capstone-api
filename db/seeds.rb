042e9da83797c6c3022be0439999bdd0940c92ea
Ricardo Trevino <Rickytrevino2380@gmail.com>
2024-02-26 17:00:02 -0800

    add db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Product.create(name: "Instant Espresso", price: "5", image_url: "htt
+ps://target.scene7.com/is/image/Target/GUEST_1eabf89f-651a-40cc-89e3-365cb8e3f6b
+3?wid=488&hei=488&fmt=pjpeg", description: "dehydrated espresso, mix with hot wat
+er for instant espresso")
+Product.create(name: "Coffee", price: "1", image_url: "https://nypost.com/wp-content/uploads/sites/2/2023/01/coffee.jpg", description: "coffee made to order!")
+Product.create(name: "Criossant", price: "2", image_url: "https://www.theflavorbender.com/wp-content/uploads/2020/05/French-Croissants-SM-2363.jpg", description: "Soft, warm, and chewy Croissant")
