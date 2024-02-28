ecb790fc26e16b8bfe9296b897b34dab5b6fd8f9
Ricardo Trevino <Rickytrevino2380@gmail.com>
2024-02-27 19:03:47 -0800

    standardized, with front end capability

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @product = Product.all
+    @products = Product.all
     render :index
   end
 
@@ -9,8 +9,38 @@ class ProductsController < ApplicationController
     render :show
   end
 
-  #@product = Product.find_by(id: params["id"])
+  def create
+    @product = Product.create(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
 
+    )
+    render :show
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(
+      name: params[:name] || @product.name,
+      price: params[:price] || @product.price,
+      image_url: params[:image_url] || @product.image_url,
+      description: params[:description] || @product.description,
+    )
+    render :show
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    render json: { message: "Product sent into the abyss successfully!" }
+  end
+
+  #@product = Product.find_by(id: params["id"])
+  # #name: "Instant Espresso", price: "5", image_url: "htt
+  # ps://target.scene7.com/is/image/Target/GUEST_1eabf89f-651a-40cc-89e3-365cb8e3f6b
+  # 3?wid=488&hei=488&fmt=pjpeg", description: "dehydrated espresso, mix with hot water for instant espresso"
   #shows an array ^ notice the s
 
   # does not show as an array ^ no s
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/products", controller: "products", action: "display_products_method"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
 
   get "/products" => "products#index"
 
   get "/products/:id" => "products#show"
+
+  post "/products" => "products#create"
+
+  patch "/products/:id" => "products#update"
+
+  delete "/products/:id" => "products#destroy"
 
   get "up" => "rails/health#show", as: :rails_health_check
 
